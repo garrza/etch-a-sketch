97e2c2cd47096c5feb72a4649254239205a0682b
Ramiro Garza <garza.ramirov@gmail.com>
2023-03-20 19:06:42 -0600

    Addition of new button, implementation of new grid system

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
 const container = document.querySelector(".container")
-
-for(let i = 1;i<=16;i ++){
-    let newDiv = document.createElement("div");
-    newDiv.id = "square" + i;
-    newDiv.classList.add("square");
-    newDiv.textContent = "poop";
-    container.appendChild(newDiv);
-    console.log(newDiv);
-}
+const btn = document.querySelector(".btn");
+let dimensions = 16;
+createGrid(dimensions);
 
 const squares = document.querySelectorAll(".square")
 
+btn.addEventListener("click", () => {
+    let newDimensions = prompt("Enter a number from 1-100:");
+    alert(`The new grid is ${newDimensions} x ${newDimensions}`);
+    removeAllChildNodes(container);
+    createGrid(newDimensions);
+})
+
 squares.forEach(item => {
     item.addEventListener("mouseover", () => {
-        item.classList.add("hover")
+        item.classList.toggle("hover");
     });
 });
+
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+function createGrid(dimensions) {
+    for (let i = 1; i <= dimensions; i++) {
+        let newDiv = document.createElement("div");
+        newDiv.id = "square" + i;
+        newDiv.classList.add("square");
+        newDiv.textContent = "👻";
+        container.appendChild(newDiv);
+        console.log(newDiv);
+    }
+}
